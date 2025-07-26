a997492ac776e5bb3ffcb40b1d18ba19f74c05ed
coralsh <sukku.01k@gmail.com>
2025-07-26 14:21:45 +0530

    Added looping st

---
+++ Looping st/for.py
@@ -0,0 +1,12 @@
+print("Prime numbers between 20 and 50 are:")
+
+for num in range(20, 51): 
+    if num > 1:
+        is_prime = True
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(num, end=" ")
+
